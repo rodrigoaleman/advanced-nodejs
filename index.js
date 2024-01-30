0293842025505ddd79aa50632a8fd9ad2c516ae0
Rodrigo Aleman <rodrigoal9715@gmail.com>
2024-01-30 11:19:22 -0600

    Performanc optimize cluster mode
    Optimize cluster mode by utilizing the `cluster` module in the `index.js` file. If the file is being executed in the master mode, it will spawn child processes using `cluster.fork()`. On the other hand, if it is executed in child mode, it will act as a server and do nothing else. This change aims to improve the performance and scalability of the application.

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+//    cause index.js to ve executed *again* but in child mode
+    cluster.fork();
+} else {
+//    I'm a child, I'm going to act like a server and do noting else
+    const express = require('express');
+    const app = express();
 
+    function doWork(duration) {
+        const start = Date.now();
+        while (Date.now() - start < duration) {
+
+        }
     }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there');
+    });
+
+    app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
-});
-
-app.listen(3000);
