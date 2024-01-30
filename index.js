b05530d41c2126bb4bf82b0ff000e5db5e2f4021
Rodrigo Aleman <rodrigoal9715@gmail.com>
2024-01-30 12:17:41 -0600

    Remove cluster related logic in index.js and add encryption functionality
    * Removed the cluster module and child process forking logic in index.js to simplify the codebase.
    * Added crypto module to support encryption functionality using pbkdf2 algorithm.
    * Updated the / route to perform encryption using crypto.pbkdf2() function before sending the response.
    * Removed unused doWork() function.
    * Added a new /fast route to send a fast response.
    * Fixed missing newline at the end of the file.

--- index.js
+++ index.js
@@ -1,32 +1,16 @@
-const cluster = require('cluster');
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-//    cause index.js to ve executed *again* but in child mode
-    cluster.fork();
-//    cluster.fork();
-//    cluster.fork();
-//    cluster.fork();
-} else {
-//    I'm a child, I'm going to act like a server and do noting else
-    const express = require('express');
-    const app = express();
 
-    function doWork(duration) {
-        const start = Date.now();
-        while (Date.now() - start < duration) {
-
-        }
-    }
-
-    app.get('/', (req, res) => {
-        doWork(5000);
+app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
         res.send('Hi there');
     });
+});
 
-    app.get('/fast', (req, res) => {
-        res.send('This was fast!');
-    });
+app.get('/fast', (req, res) => {
+    res.send('This was fast!');
+});
 
-    app.listen(3000);
-}
+app.listen(3000);
